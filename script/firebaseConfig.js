0037f1ddb804c20505da1f453e6af6bbcc727701
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-10-26 20:30:56 +0000

    add a firebase config file

---
+++ script/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgdDRikWVnieInU_C6GLRQxL_u8mwuqxI",
+  authDomain: "nourishwise-fdc37.firebaseapp.com",
+  projectId: "nourishwise-fdc37",
+  storageBucket: "nourishwise-fdc37.appspot.com",
+  messagingSenderId: "948685887114",
+  appId: "1:948685887114:web:6fa1ab201e36680e7cff73",
+  measurementId: "G-TJENJWS1JX"
+};
+export { firebaseConfig };
