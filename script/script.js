655fd2b1934309b8b87c4b28c11ced834d951937
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-10-31 17:40:26 +0000

    make the nav bar more responsive

--- script/script.js
+++ script/script.js
@@ -1,21 +1,9 @@
-// function toggleNavbar() {
-//   const header = document.getElementById('header');
-//   const mainContent = document.getElementById('main-Container');
-//   const navList = document.getElementById('nav-list');
-//   const mmainContent = document.getElementById('mainContainer');
+// // Function to open and close side bar
+const main_content = document.getElementById('mainContainer')
+function openNavPanel() {
+  document.getElementById("nav-list").style.left = "0";
+}
 
-//   // Check the current height of the header to determine its state
-//   if (header.style.height === '4rem' || header.style.height === '') {
-//     // Open the navbar
-//     header.style.height = '5rem'; // Adjust height as needed
-//     navList.style.display = 'block'; // Ensure the nav list is visible when open
-//     mainContent.style.marginTop = '200px';
-//     mmainContent.style.marginTop = '200px'; // Uncomment if you have multiple main containers to shift
-//   } else {
-//     // Close the navbar
-//     header.style.height = '4rem'; // Change this to whatever the closed height should be (e.g., the height of the logo and menu icon)
-//     navList.style.display = 'none'; // Hide the nav list when closed
-//     mainContent.style.marginTop = '0';
-//     mmainContent.style.marginTop = '2rem';
-//   }
-// }
+function closeNavPanel() {
+  document.getElementById("nav-list").style.left = "-250px";
+}
